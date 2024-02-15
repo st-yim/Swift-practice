51116776ebe30c35ed29fbbc8b7593cabbf97f4a
Steven <styim@seas.upenn.edu>
2024-02-14 21:33:26 -0800

    Add playground day11 file.

--- Swift_practice_day11.playground/Contents.swift
+++ Swift_practice_day11.playground/Contents.swift
@@ -1,6 +1,7 @@
 import UIKit
 
-/// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise). You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+/// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise). 
+/// Note: You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
 
 class Solution {
     func rotate(_ matrix: inout [[Int]]) {
